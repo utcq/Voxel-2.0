925787dce63ab464df9ea06379de8b70c9a00aeb
UnityTheCoder <108679939+UnityTheCoder@users.noreply.github.com>
2022-11-05 15:10:33 +0100

    Update install.sh

--- assets/install.sh
+++ assets/install.sh
@@ -1,4 +1,4 @@
-sudo rm -r /usr/lib/voxel; exit 0
+sh -c "sudo rm -r /usr/lib/voxel; exit 0"
 sudo git clone https://github.com/UnityTheCoder/Voxel-2.0 /usr/lib/voxel
-sudo rm /usr/bin/vxc; exit 0
+sh -c "sudo rm /usr/bin/vxc; exit 0"
 sudo ln -s /usr/lib/voxel/voxel /usr/bin/vxc
