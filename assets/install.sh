cf57e05b8fef02240caa86af8ecdb55e9ecc95f5
UnityTheCoder <108679939+UnityTheCoder@users.noreply.github.com>
2022-11-19 19:45:30 +0100

    Update install.sh

--- assets/install.sh
+++ assets/install.sh
@@ -1,5 +1,6 @@
 sudo git clone https://github.com/UnityTheCoder/Voxel-2.0 /usr/lib/voxel
 python3 -m pip install toml
+python3 -m pip install rich
 sudo ln -s /usr/lib/voxel/voxel /usr/bin/vxc
 sudo chmod +x /usr/bin/vxc
 sudo ln -s /usr/lib/voxel/vix /usr/bin/vix
